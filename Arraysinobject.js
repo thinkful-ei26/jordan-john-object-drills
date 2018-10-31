13bdb5fbb5adbf61616f0014bdebcc800f60c6f7
jnguyen77 <Johnsum41.1@gmail.com>
2018-10-31 14:33:05 -0400

    drill complete

---
+++ Arraysinobject.js
@@ -0,0 +1,11 @@
+// 3. Arrays in objects
+// Create an object with a property called meals which is an array of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+
+const hobbit ={
+    meals:['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+};
+
+console.log(hobbit.meals[3]);
+    
