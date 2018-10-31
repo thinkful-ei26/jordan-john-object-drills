b1a9abe381d90b3e12378c6b8565209f9a0413d0
Jordan Haddadi <jordanhaddadi@gmail.com>
2018-10-31 12:19:46 -0400

    working on object updater

---
+++ objectupdater.js
@@ -0,0 +1,22 @@
+// Modify the updateObject function (which takes a single argument (obj)) so that it adds the following key/value pairs to obj and returns it:
+
+// foo => 'foo'
+// bar => 'bar'
+// bizz => 'bizz'
+// bang => 'bang'
+
+
+function updateObject(myObj) {
+
+   myObj.foo = 'foo';
+   myObj.bar = 'bar';
+   myObj.bizz = 'bizz';
+   myObj.bang = 'bang';
+ 
+return myObj;
+
+}
+
+console.log(updateObject(myObj))
+
+
