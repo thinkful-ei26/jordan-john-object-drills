223de05af304b49b37357248507829eae9bd46d6
jnguyen77 <Johnsum41.1@gmail.com>
2018-10-31 12:08:11 -0400

    objectd

---
+++ objectdrills.js
@@ -0,0 +1,22 @@
+
+//Object creator
+
+function createMyObject(){
+    const myobject ={
+
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function(){
+        return 'hello'
+    } 
+};
+    return myobject;
+}
+
+console.log(createMyObject());
+
+
+
+
+
