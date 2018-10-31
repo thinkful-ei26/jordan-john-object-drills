c964b23fe582b50c9b042911b3eb2aa14247f647
jnguyen77 <Johnsum41.1@gmail.com>
2018-10-31 16:13:33 -0400

    drill complete

--- crackingthecode.js
+++ crackingthecode.js
@@ -1,41 +1,30 @@
 
-//Cracking the code
-
-let message = 'craft block argon meter bells brown croon droop'
-
-const messagesplit = message.split (' ')
-
 let decipher = {
-    a: 2,
-    b: 3,
-    c: 4,
-    d: 5,
-};
+    a : 2,
+    b : 3,
+    c : 4,
+    d : 5
+  };
+  
+  let Message = 'craft block argon meter bells brown croon droop';
+  
+  function decodeWord(word){
+    let decipherNumber = decipher[word[0]];
+    if(decipherNumber === undefined){
+      return ' ';
+    } else
+    return word[decipherNumber - 1];
+  }
 
-for (let i = 0; i < messagesplit.length; i++){
-    if (decipher[messagesplit[i]]) {
-        return messagesplit[decipher[messagesplit[i]]-1];
-    } else {
-        return ' ';
+  console.log(decodeWord('craft'))
+  
+  function decode(string){
+    let crackcode = '';
+    let wordArray = string.split(' ');
+    for(let i = 0; i < wordArray.length; i++){
+      crackcode += decodeWord(wordArray[i]);
     }
-}
-console.log(messagesplit[i]);
-
-// let decode = (encodedWord) => {
-//     if (decipher[encodedWord[0]]) {
-//         return encodedWord[decipher[encodedWord[0]]-1];
-//     } else {
-//         return ' ';
-//     }
-
-// }
-
-// should return correct decoded character
-
-// console.log(decode('craft '));
-
-// let decodeWords = (message) => {
-//     let message2= message.split (' ');
-//     return message2.reduce()
-// }
-
+    return crackcode;
+  }
+  
+  console.log(decode(Message));
